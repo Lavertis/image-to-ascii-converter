e7d4da3a1f0d0c9296d8e92804683e12e8938864
Lavertis <29688908+Lavertis@users.noreply.github.com>
2020-09-14 01:21:32 +0200

    Fix - scale image to 40 pixels less

--- photo.py
+++ photo.py
@@ -8,9 +8,10 @@ from character_map import *
 
 def get_pixmap(path):
     im = Image.open(path)
-    if im.size[0] > 200 and im.size[0] > 200:
+    size = 160
+    if im.size[0] > size and im.size[0] > size:
         height_to_width_ratio = im.size[1] / im.size[0]
-        pixels = 200
+        pixels = size
         resized_image = im.resize((pixels, int(pixels * height_to_width_ratio)))
         pixmap = resized_image.load()
         pixmap_size = resized_image.size
